45478de652b0b9a980e23b6aac414905bbb22bdd
Michael Lucky <luckymichael00@gmail.com>
2024-03-22 23:59:42 -0400

    first basic changes and first real commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <div className="App">
+            <>
+                < Card />
+                <div>
+                    I'm using this showcase as a place to post weekly / bi-weekly projects to deepen my knowledge of 
+                    coding + software development + computer science. I will be using this as a place to post my 
+                    projects and keep myself accountable to make continuous progress to become an expert in the field
+                    and get hired.
+                </div>
+
+                <ul>
+                    Some ideas planned for the future:
+                    <li>Exercise Routine App</li>
+                    <li>Chrome Extension to make twitch.tv only show the people you follow who are live</li>
+                    <li>Chess Game</li>
+                    <li>Fourier Series Visualization</li>
+                    <li>2D javascript game</li>
+                </ul>
+            </>
+        </div>
+    );
 }
-
-export default App;
