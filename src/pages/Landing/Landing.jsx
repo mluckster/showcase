fb6f28ace348d197169b7aced514d7f81ca1fed1
Michael Lucky <luckymichael00@gmail.com>
2024-05-03 14:57:01 -0400

    favicon, content update

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -15,9 +15,8 @@ export default function Landing({ setLanding, landing }){
                     Welcome to my Project Blog
                 </div>
                 <div>
-                    I will be making posts on this site for any new projects I create in my quest to find full time employment in the software development space. Each post will be accompanied with a small blurb based on what I learned from the development process, any challenges, what I would want to try differently, or anything along those lines.
+                    I will be making posts on this site for any new projects I create in my quest to find full time employment in the software development space. Each post will be accompanied with a small blurb based on what I learned from the development process, any challenges, what I would want to do differently, or anything else about the project building experience.
                 </div>
-                <div>Enter the site to check things out!</div>
             </div>
             <div className="enter" onClick={handleEnter}>ENTER</div>    
         </>
